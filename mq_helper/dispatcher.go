8f5b1f300f8b20f55c827eca224c4c0c1874fafd
seagullbird <seagullbird823@gmail.com>
2018-02-12 21:39:31 +0800

    added mq_helper package

---
+++ mq_helper/dispatcher.go
@@ -0,0 +1,80 @@
+package mq_helper
+
+import (
+	"github.com/streadway/amqp"
+	"github.com/go-kit/kit/log"
+	"encoding/json"
+	"github.com/seagullbird/headr-common/config"
+)
+
+type Dispatcher interface {
+	DispatchMessage(message interface{}) (err error)
+}
+
+type AMQPDispatcher struct {
+	channel       	*amqp.Channel
+	queueName     	string
+	mandatorySend 	bool
+	logger 			log.Logger
+}
+
+func (d *AMQPDispatcher) DispatchMessage(message interface{}) (err error) {
+	d.logger.Log("Dispatching message to queue", d.queueName)
+	body, err := json.Marshal(message)
+	if err == nil {
+		err = d.channel.Publish(
+			"",              // exchange
+			d.queueName,     // routing key
+			d.mandatorySend, // mandatory
+			false,           // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		if err != nil {
+			d.logger.Log("Failed to dispatch message, err", err)
+		}
+	} else {
+		d.logger.Log("Failed to marshal err", err, "message", message)
+	}
+	return
+}
+
+func NewDispatcher(queueName string, logger log.Logger) Dispatcher {
+	uri := amqp.URI{
+		Scheme:   "amqp",
+		Host:     config.MQSERVERNAME,
+		Port:     5672,
+		Username: "user",
+		Password: config.MQSERVERPWD,
+		Vhost:    "/",
+	}
+	conn, err := amqp.Dial(uri.String())
+	if err != nil {
+		logger.Log("Failed to connect to RabbitMQ", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		logger.Log( "Failed to open a channel", err)
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName, 			// name
+		false,		// durable
+		false,	// delete when unused
+		false,		// exclusive
+		false,		// no-wait
+		nil,			// arguments
+	)
+	if err != nil {
+		logger.Log( "Failed to declare a queue", err)
+	}
+
+	return &AMQPDispatcher{
+		channel: ch,
+		queueName: q.Name,
+		mandatorySend: false,
+		logger: logger,
+	}
+}
---
+++ mq_helper/receiver.go
@@ -0,0 +1,75 @@
+package mq_helper
+
+import (
+	"github.com/go-kit/kit/log"
+	"github.com/streadway/amqp"
+	"github.com/seagullbird/headr-common/config"
+	"reflect"
+)
+
+type Receiver interface {
+	RegisterListener(queueName string, event interface{}, listener Listener)
+}
+
+type AMQPReceiver struct {
+	ch 				*amqp.Channel
+	logger 			log.Logger
+	registration	map[string]Listener
+}
+
+// Listener is a function that takes action when an event is received.
+type Listener func(delivery amqp.Delivery, logger log.Logger)
+
+func (r *AMQPReceiver) RegisterListener(queueName string, listener Listener) {
+	if l, ok := r.registration[queueName]; ok {
+		r.logger.Log("Listener already registered", reflect.ValueOf(l))
+		return
+	}
+	r.registration[queueName] = listener
+
+	q, _ := r.ch.QueueDeclare(
+		queueName, 		// name
+		false,          // durable
+		false,		// delete when usused
+		false,			// exclusive
+		false,			// no-wait
+		nil,				// arguments
+	)
+	qIn, _ := r.ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	// Start listening
+	go func() {
+		for d := range qIn {
+			listener(d, r.logger)
+		}
+	}()
+}
+
+func NewReceiver(logger log.Logger) *AMQPReceiver {
+	uri := amqp.URI{
+		Scheme:   "amqp",
+		Host:     config.MQSERVERNAME,
+		Port:     5672,
+		Username: "user",
+		Password: config.MQSERVERPWD,
+		Vhost:    "/",
+	}
+	conn, err := amqp.Dial(uri.String())
+
+	ch, err := conn.Channel()
+	if err != nil {
+		logger.Log("Failed to open AMQP channel", err)
+	}
+	return &AMQPReceiver{
+		ch: ch,
+		logger:	logger,
+	}
+}
